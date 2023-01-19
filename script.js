7cec7b6363f0edda0a40b1de1a410097b838223c
Nilesh <nilesh@haachico.com>
2023-01-19 20:27:30 +0530

    fix eye bug

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 const passwordBox = document.querySelector(".password-box");
-const eyeIconBtn = document.querySelector(".eye-icon");
+const openEyeBtn = document.querySelector(".eye-icon");
 const closeEyeBtn = document.querySelector(".closeEye-icon");
 
-closeEyeBtn.addEventListener("click", function () {
+openEyeBtn.addEventListener("click", function () {
   if (passwordBox.type === "password") {
     passwordBox.type = "text";
-    eyeIconBtn.style.display = "block";
-    closeEyeBtn.style.display = "none";
+    openEyeBtn.style.display = "none";
+    closeEyeBtn.style.display = "block";
   }
 });
 
-eyeIconBtn.addEventListener("click", function () {
+closeEyeBtn.addEventListener("click", function () {
   if (passwordBox.type === "text") {
     passwordBox.type = "password";
-    eyeIconBtn.style.display = "none";
-    closeEyeBtn.style.display = "block";
+    openEyeBtn.style.display = "block";
+    closeEyeBtn.style.display = "none";
   }
 });

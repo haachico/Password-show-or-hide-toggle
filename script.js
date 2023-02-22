e8cbafd21ae65d1c8cf6ed53c43a2a02e5b083a5
Nilesh <nilesh@haachico.com>
2023-02-22 14:59:16 +0530

    corrected toggle functionality

--- script.js
+++ script.js
@@ -3,17 +3,28 @@ const openEyeBtn = document.querySelector(".eye-icon");
 const closeEyeBtn = document.querySelector(".closeEye-icon");
 
 openEyeBtn.addEventListener("click", function () {
-  if (passwordBox.type === "password") {
-    passwordBox.type = "text";
-    openEyeBtn.style.display = "none";
-    closeEyeBtn.style.display = "block";
+  if (passwordBox.type === "text") {
+    passwordBox.type = "password";
+    closeEyeBtn.style.visibility = "visible";
+    openEyeBtn.style.visibility = "hidden";
   }
 });
 
 closeEyeBtn.addEventListener("click", function () {
-  if (passwordBox.type === "text") {
-    passwordBox.type = "password";
-    openEyeBtn.style.display = "block";
+  if (passwordBox.type === "password") {
+    passwordBox.type = "text";
+    openEyeBtn.style.visibility = "visible";
+    closeEyeBtn.style.visibility = "hidden";
+  }
+});
+
+passwordBox.addEventListener("keyup", function () {
+  console.log(passwordBox.value);
+  if (passwordBox.value === "") {
     closeEyeBtn.style.display = "none";
+    openEyeBtn.style.display = "none";
+  } else {
+    closeEyeBtn.style.display = "block";
+    openEyeBtn.style.display = "block";
   }
 });
